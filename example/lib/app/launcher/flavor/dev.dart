ea648d007ce6a9853940664f1955ff4aa1db3670
TanBE <947624811@qq.com>
2025-01-06 22:44:19 +0800

    123

--- example/lib/app/launcher/flavor/dev.dart
+++ example/lib/app/launcher/flavor/dev.dart
@@ -1,6 +1,9 @@
+import 'package:flutter/cupertino.dart';
+
 import '../../app_launcher.dart';
 import '../strategy/dev_launcher_strategy.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   AppLauncher.launch(DevLauncherStrategy());
 }
