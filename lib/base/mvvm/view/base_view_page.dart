149a19566ed26fb9fbb31a1306588e7c8f0d367b
Tennysion <16626248492@163.com>
2025-02-28 10:28:37 +0800

    provider

--- lib/base/mvvm/view/base_view_page.dart
+++ lib/base/mvvm/view/base_view_page.dart
@@ -1,6 +1,7 @@
 import 'package:common/base/mvvm/view/base_view_interface.dart';
 import 'package:common/base/mvvm/vm/base_view_model.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 abstract class BaseViewPage<c extends BaseViewModel> extends StatelessWidget
     with BaseViewInterface<c> {
@@ -10,7 +11,10 @@ abstract class BaseViewPage<c extends BaseViewModel> extends StatelessWidget
 
   @override
   Widget build(BuildContext context) {
-    return buildContent(context, buildController(context));
+    return ChangeNotifierProvider(
+      create: (context) => buildController(context),
+      child: buildContent(context, controller),
+    );
   }
 
   @override
